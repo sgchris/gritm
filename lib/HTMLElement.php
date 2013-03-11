6d210d1a3244277361162526230e56b6aa742cd0
Gregory Chris <sgchris@gmail.com>
2013-03-11 18:24:18 +0200

    added CSS and javascript support to the base element class

--- lib/HTMLElement.php
+++ lib/HTMLElement.php
@@ -12,10 +12,34 @@ class HTMLElement {
 	private $_html;
 
 	/**
+	 * The css of the element
+	 */
+	private $_css;
+
+	/**
+	 * The Javascript of the element
+	 */
+	private $_javascript;
+
+	/**
 	 * Get the html of the element
 	 */
 	public function getHtml() {
 		return $this->_html;
+	}
+
+	/**
+	 * Get the css of the element
+	 */
+	public function getCss() {
+		return $this->_css;
+	}
+
+	/**
+	 * Get the javascript of the element
+	 */
+	public function getJavascript() {
+		return $this->_javascript;
 	}
 
 	/**
@@ -35,6 +59,46 @@ class HTMLElement {
 	 */
 	private function setHtml($html) {
 		$this->_html=$html;
+		return $this;
+	}
+
+	/**
+	 * add css to the current css
+	 * @param $css string
+	 * @return this object
+	 */
+	private function appendCss($css) {
+		$this->_css.=$css;
+		return $this;
+	}
+
+	/**
+	 * set css to the current css
+	 * @param $css string
+	 * @return this object
+	 */
+	private function setCss($css) {
+		$this->_css=$css;
+		return $this;
+	}
+
+	/**
+	 * add javascript to the current javascript
+	 * @param $javascript string
+	 * @return this object
+	 */
+	private function appendJavascript($javascript) {
+		$this->_javascript.=$javascript;
+		return $this;
+	}
+
+	/**
+	 * set javascript to the current javascript
+	 * @param $javascript string
+	 * @return this object
+	 */
+	private function setJavascript($javascript) {
+		$this->_javascript=$javascript;
 		return $this;
 	}
 
