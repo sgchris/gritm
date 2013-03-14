aaa6f6c91242c57b3bfc9e360b2443cef85f43ee
sgchris <sgchris@gmail.com>
2013-03-14 10:20:01 +0200

    Request object

--- lib/Tools/Request.php
+++ lib/Tools/Request.php
@@ -4,6 +4,12 @@
  */
 
 class Request {
+
+	/**
+	 * the base dir of the app (relative path)
+	 * The value has to be changed when moving the file (Request.php) to another location
+	 */
+	protected $_APP_BASE_DIR;
 	
 	/**
 	 * local var for GET request parameters
@@ -21,20 +27,58 @@ class Request {
 	protected $_files;
 
 	/**
+	 * parsed URL request. /path/to/page
+	 * $urlParams = array('0'=>'path', '1'=>'to', '2'=>'url')
+	 */
+	protected $_urlParams = null;
+
+	/**
+	 * The relative path from doc root (`htdocs` or `www` dir)
+	 */
+	protected $_relativePath = null;
+
+	/**
+	 * get GET parameter
+	 */
+	public function get($idx) {
+		return isset($this->_get[$idx]) ? $this->_get[$idx] : null;
+	}
+
+	/**
+	 * get POST parameter
+	 */
+	public function post($idx) {
+		return isset($this->_post[$idx]) ? $this->_post[$idx] : null;
+	}
+
+	/**
+	 * get GET parameter
+	 */
+	public function file($idx) {
+		return isset($this->_files[$idx]) ? $this->_files[$idx] : null;
+	}
+
+
+	/**
 	 * determine if the current request is AJAX
 	 */	
-	protected $_isAjax;
+	protected $_isAjax = false;
 
 	/**
 	 * initialize the request object
+	 * - Parse the current request (_POST | _GET | _FILES)
+	 * - check of the request is AJAX
 	 */
 	public function __construct() {
 		$this->_get = $_GET;
 		$this->_post = $_POST;
 		$this->_files = $_FILES;
 
-		// determine wether this is an AJAX call
-		$this->_isAjax = false; /////////////////////////////////////////// TODO!!!!!
+		// set the base APP dir
+		$this->_APP_BASE_DIR = __DIR__.'/../..';
+
+		// check if this is an AJAX call
+		$this->_isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']); 
 	}	
 
 	/**
@@ -45,9 +89,62 @@ class Request {
 	}
 
 	/**
-	 * Parse the current request (_POST | _GET | _FILES)
+	 * parse URL request. /path/to/page
+	 * $urlParams = array('0'=>'path', '1'=>'to', '2'=>'url')
 	 */
 	public function parse() {
+		$uri = $_SERVER['REQUEST_URI'];
 
+		// remove the request GET parameters 
+		$uri = ($qMarkPos = strpos($uri, '?')) !== false ? substr($uri, 0, $qMarkPos) : $uri;
+
+		// remove the leading base path
+		$uri = str_replace($this->getRelativePath(), '', $uri);
+
+		// remove slashes
+		$uri = trim($uri, '/');
+
+		$this->_urlParams = explode('/', $uri);
 	}
+
+	/**
+	 * Get URL param. When the request is /path/to/file. 0->path 1->to 2->file
+	 */
+	public function getUrlParam($number) {
+		if (is_null($this->_urlParams)) {
+			$this->parse();
+		}
+
+		return isset($this->_urlParams[$number]) ? $this->_urlParams[$number] : null;
+	}
+
+	/**
+	 * Get the relative path from doc root (`htdocs` or `www` dir)
+	 */
+	public function getRelativePath() {
+
+		if (is_null($this->_relativePath)) {
+
+			// get app path and the docRoot path
+			$docRoot = realpath($_SERVER['DOCUMENT_ROOT']);
+
+			// get the base dir for the app
+			$baseDir = realpath($this->_APP_BASE_DIR);
+
+			// substruct the base path from the app path
+			$baseDir = substr($baseDir, strlen($docRoot));
+
+			// convert win slashes to URL slashes
+			$baseDir = str_replace('\\', '/', $baseDir);
+
+			// remove the trailing slash
+			$baseDir = rtrim($baseDir, '/');
+
+			// assign the base dir to the local var
+			$this->_relativePath = $baseDir;
+		}
+
+		return $this->_relativePath;
+	}
+
 }
