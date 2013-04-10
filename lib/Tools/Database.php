5c8173d377b66470358d09361e52ee5b8d002ecb
Gregory Chris <sgchris@gmail.com>
2013-04-10 15:13:57 +0300

    Created insert, update, delete functions in the database class

--- lib/Tools/Database.php
+++ lib/Tools/Database.php
@@ -51,26 +51,122 @@ class Database {
      * @param type $tableName
      * @param type $fields
      */
-    public function insert($tableName, $fields) {
-        
+    public function insert($tableName, array $fields) {
+        if (empty($fields)) {
+            throw new Exception('error : no fields parameters in ' . __METHOD__);
+        }
+
+        // get the list of fields (names)
+        $fields = array();
+        $values = array();
+        foreach (array_keys($fields) as $fieldName) {
+            $fields[] = '`' . $fieldName . '`';
+            $values[] = ':' . $fieldName;
+        }
+
+        // create the SQL string
+        $sql = 'insert into `' . $tableName . '` (' . implode(',', $fields) . ') values (' . implode(',', $values) . ')';
+
+        // bind values
+        $db = self::getInstance();
+        $stmt = $db->prepare($sql);
+        foreach ($fields as $key => $value) {
+            $stmt->bindValue(':' . $key, $value);
+        }
+
+        // execute the query
+        $stmt->execute();
+        return $db->lastInsertId();
     }
-    
+
     /**
      * Update a record in the database
      * @param type $tableName
      * @param type $fields
      * @param type $whereFields
      */
-    public function update($tableName, $fields, $whereFields) {
-        
+    public function update($tableName, array $fields, array $whereFields) {
+        if (empty($fields)) {
+            throw new Exception('error : no fields parameters in ' . __METHOD__);
+        }
+
+        // create fields counter in case where the same field is in SET and in WHERE
+        $fieldsCoutner = 0;
+
+        // init the sql string
+        $sql = "update `$tableName` set ";
+
+        // gather "SET" sql section variables
+        $setValues = array();
+        foreach (array_keys($fields) as $fieldName) {
+            $setValues[] = '`' . $fieldName . '` = :' . $fieldName . ($fieldsCoutner++);
+        }
+
+        // add the "SET" clause to the SQL
+        $sql .= implode(',', $setValues);
+
+        // build the 'where' clause
+        $whereConds = array();
+        foreach (array_keys($whereFields) as $fieldName) {
+            $whereConds[] = '`' . $fieldName . '` = :' . $fieldName . ($fieldsCoutner++);
+        }
+
+        if (!empty($whereConds)) {
+            $sql .= ' WHERE (' . implode(') AND (', $whereConds) . ')';
+        }
+
+        // prepare the statement
+        $db = self::getInstance();
+        $stmt = $db->prepare($sql);
+
+        // bind values for the SET
+        $fieldsCoutner2 = 0;
+        foreach ($fields as $fieldName => $fieldValue) {
+            $stmt->bindValue(':' . $fieldName . ($fieldsCoutner2++), $fieldValue);
+        }
+
+        // bind values for the WHERE
+        foreach ($whereFields as $fieldName => $fieldValue) {
+            $stmt->bindValue(':' . $fieldName . ($fieldsCoutner2++), $fieldValue);
+        }
+
+        // execute the query
+        $stmt->execute();
+        return $this;
     }
-    
+
     /**
      * Delete a record from the database
      * @param type $tableName
      * @param type $whereFields
      */
-    public function delete($tableName, $whereFields) {
-        
+    public function delete($tableName, array $whereFields) {
+
+        // get the list of fields (names)
+        $sql = 'delete from `' . $tableName . '`';
+
+        // build the 'where' clause
+        $whereConds = array();
+        foreach (array_keys($whereFields) as $fieldName) {
+            $whereConds[] = '`' . $fieldName . '` = :' . $fieldName . ($fieldsCoutner++);
+        }
+        // create the SQL string
+        if (!empty($whereConds)) {
+            $sql.= ' where (' . implode(') and (', $whereConds) . ')';
+        }
+
+        // prepare the statement
+        $db = self::getInstance();
+        $stmt = $db->prepare($sql);
+
+        // bind values for the WHERE
+        foreach ($whereFields as $fieldName => $fieldValue) {
+            $stmt->bindValue(':' . $fieldName, $fieldValue);
+        }
+
+        // execute the query
+        $stmt->execute();
+        return $this;
     }
+
 }
